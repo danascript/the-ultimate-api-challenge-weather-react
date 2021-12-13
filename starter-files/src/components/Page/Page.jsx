09882c46aa8c3e22615a47f7235f42b5798312b4
jwiergowska <85780659+jwiergowska@users.noreply.github.com>
2021-12-13 14:39:11 +0100

    updated

--- starter-files/src/components/Page/Page.jsx
+++ starter-files/src/components/Page/Page.jsx
@@ -1,11 +1,31 @@
 import React, { Fragment } from 'react';
 
 import Header from '../Header';
+import Form from '../Form';
+import Error from '..Error';
+import Loader from '..Loader';
+import Forecast from '..Forecast';
+
+import styles from '/.Page.module.css'
+
 
 const Page = () => {
     return (
         <Fragment>
             <Header />
+        <div className={styles.box}>
+        
+        {/* Form */}
+<Form />
+        {/* Error */}
+
+{/* Loader */}
+
+
+{/* Forecast */}
+        
+        
+        </div>
         </Fragment>
     );
 };
