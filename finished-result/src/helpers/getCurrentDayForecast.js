8a81c0ba28cd093281600c32b68984c50d50518f
Daniela <daniela.fedyakin@gmail.com>
2022-03-22 14:01:26 +0100

    set strict versioning for packages, fix icon url

--- finished-result/src/helpers/getCurrentDayForecast.js
+++ finished-result/src/helpers/getCurrentDayForecast.js
@@ -5,7 +5,7 @@ const getCurrentDayForecast = (data, title) => ({
     date: moment(data.applicable_date).format('MMMM Do'),
     location: title,
     temperature: Math.round(data.the_temp),
-    weatherIcon: `'https://www.metaweather.com/static/img/weather/${data.weather_state_abbr}.svg`,
+    weatherIcon: `https://www.metaweather.com/static/img/weather/${data.weather_state_abbr}.svg`,
     weatherDescription: data.weather_state_name,
 });
 
